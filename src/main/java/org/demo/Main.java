adcba646f7c5f2c35310ea095235c80e56d99fb0
brcadal <brucadal@gmail.com>
2025-07-08 08:08:35 +0700

    Model Design

--- src/main/java/org/demo/Main.java
+++ src/main/java/org/demo/Main.java
@@ -1,7 +1,13 @@
 package org.demo;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello from a separate thread (old way)");
+            }
+        });
     }
 }
+
