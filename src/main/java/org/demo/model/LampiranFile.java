d9b71bf8f41ce7eec7ef4bde26d223e9c3fa08c1
brcadal <brucadal@gmail.com>
2025-07-11 14:30:14 +0700

    Lampiran File Model

--- src/main/java/org/demo/model/LampiranFile.java
+++ src/main/java/org/demo/model/LampiranFile.java
@@ -1,4 +1,12 @@
 package org.demo.model;
 
 public class LampiranFile {
+    private String namaFile;
+    private String url;
+
+    public LampiranFile(){}
+    public LampiranFile(String namaFile, String url){
+        this.namaFile = namaFile;
+        this.url = url;
+    }
 }
