4288254941ee1bcc91eb713d58281fe7ac42df7b
brcadal <brucadal@gmail.com>
2025-07-11 22:06:12 +0700

    Pemeriksaan fisik getter and setter is done

--- src/main/java/org/demo/model/PemeriksaanFisik.java
+++ src/main/java/org/demo/model/PemeriksaanFisik.java
@@ -19,4 +19,25 @@ public class PemeriksaanFisik {
         this.tinggiBadan = tinggiBadan;
         this.catatanTambahan = catatanTambahan;
     }
+
+    public String getTekananDarah(){return tekananDarah;}
+    public void setTekananDarah(String tekananDarah){this.tekananDarah = tekananDarah;}
+
+    public double getSuhuTubuh(){return suhuTubuh;}
+    public void setSuhuTubuh(double suhuTubuh){this.suhuTubuh = suhuTubuh;}
+
+    public String getNadi(){return nadi;}
+    public void setNadi(String nadi){this.nadi = nadi;}
+
+    public String getRespirasi(){return respirasi;}
+    public void setRespirasi(String respirasi){this.respirasi = respirasi;}
+
+    public int getBeratBadan(){return beratBadan;}
+    public void setBeratBadan(int beratBadan){this.beratBadan = beratBadan;}
+
+    public int getTinggiBadan(){return tinggiBadan;}
+    public void setTinggiBadan(int tinggiBadan){this.tinggiBadan = tinggiBadan;}
+
+    public String getCatatanTambahan(){return catatanTambahan;}
+    public void setCatatanTambahan(String catatanTambahan){this.catatanTambahan = catatanTambahan;}
 }
