c6b5a7b6b054982c025a3c1c56287ac102870dac
brcadal <brucadal@gmail.com>
2025-07-11 14:28:00 +0700

    Pemeriksaan Fisik Model

--- src/main/java/org/demo/model/PemeriksaanFisik.java
+++ src/main/java/org/demo/model/PemeriksaanFisik.java
@@ -1,4 +1,22 @@
 package org.demo.model;
 
 public class PemeriksaanFisik {
+    private String tekananDarah;
+    private double suhuTubuh;
+    private String nadi;
+    private String respirasi;
+    private int beratBadan;
+    private int tinggiBadan;
+    private String catatanTambahan;
+
+    public PemeriksaanFisik(){}
+    public PemeriksaanFisik(String tekananDarah, double suhuTubuh, String nadi, String respirasi, int beratBadan, int tinggiBadan, String catatanTambahan){
+        this.tekananDarah = tekananDarah;
+        this.suhuTubuh = suhuTubuh;
+        this.nadi = nadi;
+        this.respirasi = respirasi;
+        this.beratBadan = beratBadan;
+        this.tinggiBadan = tinggiBadan;
+        this.catatanTambahan = catatanTambahan;
+    }
 }
