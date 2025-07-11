2a249ca015cf56eeb167b4a345f5a1f5009c0191
brcadal <brucadal@gmail.com>
2025-07-11 14:23:41 +0700

    Diagnosis Model

--- src/main/java/org/demo/model/Diagnosis.java
+++ src/main/java/org/demo/model/Diagnosis.java
@@ -1,4 +1,12 @@
 package org.demo.model;
 
 public class Diagnosis {
+    private String kodeICD;
+    private String namaDiagnosis;
+
+    public Diagnosis(){}
+    public Diagnosis(String kodeICD, String namaDiagnosis){
+        this.kodeICD = kodeICD;
+        this.namaDiagnosis = namaDiagnosis;
+    }
 }
