1f23fd6a463275eb3d29e3134a22d0f3b8d066e6
brcadal <brucadal@gmail.com>
2025-07-11 14:33:25 +0700

    Dokter Model

--- src/main/java/org/demo/model/Dokter.java
+++ src/main/java/org/demo/model/Dokter.java
@@ -1,4 +1,24 @@
 package org.demo.model;
 
+import org.bson.types.ObjectId;
+
 public class Dokter {
+    private ObjectId id;
+    private String nipPegawai;
+    private String namaLengkap;
+    private String spesialisasi;
+    private String nomorSIP;
+    private Kontak kontak; // Re-use Kontak class
+    private boolean statusAktif;
+
+    public Dokter () {}
+    public Dokter(ObjectId id, String nipPegawai, String namaLengkap, String spesialisasi, String nomorSIP, Kontak kontak, boolean statusAktif){
+        this.id = id;
+        this.nipPegawai = nipPegawai;
+        this.namaLengkap = namaLengkap;
+        this.spesialisasi = spesialisasi;
+        this.nomorSIP = nomorSIP;
+        this.kontak = kontak;
+        this.statusAktif = statusAktif;
+    }
 }
