1108da7c2744e94da41b71955eb6c67687bd3934
brcadal <brucadal@gmail.com>
2025-07-12 10:28:59 +0700

    getter setter for obat is done

--- src/main/java/org/demo/model/Obat.java
+++ src/main/java/org/demo/model/Obat.java
@@ -55,6 +55,125 @@ public class Obat {
         this.jumlahPerKemasan = jumlahPerKemasan;
         this.stokTersedia = stokTersedia;
     }
-    public ObjectId getId  () { return id;}
-    public void setId(ObjectId id) {this.id = id;}
+    public ObjectId getId() {
+        return id;
+    }
+
+    public String getNamaObat() {
+        return namaObat;
+    }
+
+    public String getNamaMerek() {
+        return namaMerek;
+    }
+
+    public String getBentukSediaan() {
+        return bentukSediaan;
+    }
+
+    public String getKekuatan() {
+        return kekuatan;
+    }
+
+    public String getSatuanKekuatan() {
+        return satuanKekuatan;
+    }
+
+    public List<String> getKandunganAktif() {
+        return kandunganAktif;
+    }
+
+    public String getGolonganObat() {
+        return golonganObat;
+    }
+
+    public String getProdusen() {
+        return produsen;
+    }
+
+    public String getNomorIzinEdar() {
+        return nomorIzinEdar;
+    }
+
+    public Date getKadaluarsa() {
+        return kadaluarsa;
+    }
+
+    public double getHargaJual() {
+        return hargaJual;
+    }
+
+    public String getSatuanKemasan() {
+        return satuanKemasan;
+    }
+
+    public int getJumlahPerKemasan() {
+        return jumlahPerKemasan;
+    }
+
+    public int getStokTersedia() {
+        return stokTersedia;
+    }
+
+
+    // --- SETTERS ---
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public void setNamaObat(String namaObat) {
+        this.namaObat = namaObat;
+    }
+
+    public void setNamaMerek(String namaMerek) {
+        this.namaMerek = namaMerek;
+    }
+
+    public void setBentukSediaan(String bentukSediaan) {
+        this.bentukSediaan = bentukSediaan;
+    }
+
+    public void setKekuatan(String kekuatan) {
+        this.kekuatan = kekuatan;
+    }
+
+    public void setSatuanKekuatan(String satuanKekuatan) {
+        this.satuanKekuatan = satuanKekuatan;
+    }
+
+    public void setKandunganAktif(List<String> kandunganAktif) {
+        this.kandunganAktif = kandunganAktif;
+    }
+
+    public void setGolonganObat(String golonganObat) {
+        this.golonganObat = golonganObat;
+    }
+
+    public void setProdusen(String produsen) {
+        this.produsen = produsen;
+    }
+
+    public void setNomorIzinEdar(String nomorIzinEdar) {
+        this.nomorIzinEdar = nomorIzinEdar;
+    }
+
+    public void setKadaluarsa(Date kadaluarsa) {
+        this.kadaluarsa = kadaluarsa;
+    }
+
+    public void setHargaJual(double hargaJual) {
+        this.hargaJual = hargaJual;
+    }
+
+    public void setSatuanKemasan(String satuanKemasan) {
+        this.satuanKemasan = satuanKemasan;
+    }
+
+    public void setJumlahPerKemasan(int jumlahPerKemasan) {
+        this.jumlahPerKemasan = jumlahPerKemasan;
+    }
+
+    public void setStokTersedia(int stokTersedia) {
+        this.stokTersedia = stokTersedia;
+    }
 }
