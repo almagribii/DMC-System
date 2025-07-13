07ca05d2b27e168b04f3b5921b4624dc349539e7
brcadal <brucadal@gmail.com>
2025-07-14 05:42:44 +0700

    add dependency for spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("java")
     id("application")
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.springframework.boot") version "3.3.1"
 }
 
 application{
@@ -20,6 +22,9 @@ dependencies {
     implementation("org.mongodb:mongodb-driver-sync:4.11.1")
     implementation ("org.slf4j:slf4j-api:1.7.36")
     runtimeOnly ("ch.qos.logback:logback-classic:1.2.11")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
 }
 
 tasks.test {
