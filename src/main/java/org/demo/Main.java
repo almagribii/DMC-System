74fb9f6295f4154aeddb7c9fad8d82eabd0cd18f
brcadal <brucadal@gmail.com>
2025-07-14 05:44:38 +0700

    Main class config spring

--- src/main/java/org/demo/Main.java
+++ src/main/java/org/demo/Main.java
@@ -1,13 +1,12 @@
 package org.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args){
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from a separate thread (old way)");
-            }
-        });
+        SpringApplication.run(Main.class, args);
     }
 }
 
